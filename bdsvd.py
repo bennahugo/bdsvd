ad5457f9e87f5a22205b1cadd74a71be1b83b792
Benjamin Hugo <bennahugo@gmail.com>
2023-03-16 09:15:20 +0200

    Add MAIN table size estimation

--- bdsvd.py
+++ bdsvd.py
@@ -5,6 +5,7 @@ matplotlib.use("agg")
 import numpy as np
 import sys
 import os
+import functools
 import shutil
 import matplotlib.pyplot as plt
 from casacore.tables import table as tbl
@@ -189,6 +190,92 @@ def compression_factor(nrow, nchan, r):
     newsize = r * (nrow + nchan + 0.5)
     return origsize / newsize, origsize, newsize
 
+def data_volume_calc(num_antennas,
+                     num_polarisations,
+                     total_obs_hrs,
+                     dump_rate,
+                     num_channels,
+                     auto_correlations=True,
+                     nrows=None,
+                     num_data_cols=1,
+                     bits_per_vis = 64.0,
+                     bits_exposure = 64.0,  # double precision
+                     bits_per_flag = 8.0,
+                     bits_per_weight = 32.0,
+                     indexer_size = 32.0,
+                     time_size = 64.0,  # double precision
+                     flag_categories = 1,
+                     uvw_size = 3 * 64.0  # double precision
+                     ):
+    """Calculates the size of an MSv2, considering data columns, flags
+    (including flag category) and weights (including weight spectrum)
+    Returns in units of bytes
+
+    Adapted from SARAO calculator backend by the original author
+    """
+    # Calculate the number of rows in the MS as baselines x timestamps
+    # if not specified
+    if nrows is None:
+        baselines = num_antennas * (num_antennas - 1) / 2.0
+        if auto_correlations:
+            baselines += num_antennas
+        timestamps = total_obs_hrs * 3600 / dump_rate
+        nrows = baselines * timestamps # * (nddid = 1) * (nfield = 1) * (nobs = 1)
+
+    # CASA memo 229 (Measurement Set v2 specification)
+    # See https://casa.nrao.edu/Memos/229.html
+    # Bits used per column (including non-compulsory
+    # columns currently being dumped in
+    # https://github.com/ska-sa/katdal/blob/master/katdal/ms_extra.py)
+    # these include
+    BITS_PER_ROW = {
+        # Foreign keys
+        "ANTENNA1": indexer_size,
+        "ANTENNA2": indexer_size,
+        "ARRAY_ID": indexer_size,
+        "DATA_DESC_ID": indexer_size,
+        "FEED1": indexer_size,
+        "FEED2": indexer_size,
+        "FIELD_ID": indexer_size,
+        "OBSERVATION_ID": indexer_size,
+        "PROCESSOR_ID": indexer_size,
+        "SCAN_NUMBER": indexer_size,
+        "STATE_ID": indexer_size,
+        # complex valued data
+        "DATA": bits_per_vis * num_polarisations * num_channels,
+        # meta data
+        "EXPOSURE": bits_exposure,
+        "FLAG": bits_per_flag * num_polarisations * num_channels,
+        "FLAG_CATEGORY": bits_per_flag
+        * flag_categories
+        * num_polarisations
+        * num_channels,
+        "FLAG_ROW": bits_per_flag,
+        "WEIGHT_SPECTRUM": bits_per_weight * num_polarisations * num_channels,
+        "SIGMA_SPECTRUM": bits_per_weight * num_polarisations * num_channels,
+        "INTERVAL": bits_exposure,
+        "SIGMA": bits_per_weight * num_polarisations,
+        "WEIGHT": bits_per_weight * num_polarisations,
+        "TIME": time_size,
+        "TIME_CENTROID": time_size,
+        "UVW": uvw_size,
+    }
+    # DATA is already added
+    # add flexibility to add for instance CORRECTED_DATA and MODEL_DATA
+    # commonly needed in reductions
+    if num_data_cols == 0:
+        del BITS_PER_ROW["DATA"]
+    else:
+        for i in range(num_data_cols - 1):
+            BITS_PER_ROW[f"EXTRACOL{i}"] = BITS_PER_ROW["DATA"]
+
+    # Calculate the number of bits per column of data, flags, and weights
+    total_bits = functools.reduce(lambda a, b: a + b, BITS_PER_ROW.values())
+
+    total_data_volume = nrows * total_bits / 8
+
+    return total_data_volume
+
 def compress_datacol(VIS, DDID, FIELDID, INPUT_DATACOL, 
                      FLAGVALUE, ANTSEL, SCANSEL, CORRSEL, 
                      OUTPUTFOLDER, PLOTON, NOSKIPAUTO,
@@ -331,11 +418,13 @@ def compress_datacol(VIS, DDID, FIELDID, INPUT_DATACOL,
                 #
                 origsize = np.zeros(len(CORRSEL), dtype=np.int64)
                 newsize = np.zeros(len(CORRSEL), dtype=np.int64)
+                nrows = 0
                 for bli in svds:
                     if bli == "meta": continue
                     selbl = bl == bli
                     bla1 = a1[selbl][0]
                     bla2 = a2[selbl][0]
+                    nrows += len(bl[selbl])
                     log.info(f"\t{antnames[bla1]}&{antnames[bla2]} ({svds[bli]['bllength']:.2f} m):")
                     for cii, c in enumerate(CORRSEL):
                         ci = np.where(corrtypes == c)[0][0]
@@ -358,7 +447,28 @@ def compress_datacol(VIS, DDID, FIELDID, INPUT_DATACOL,
                     ci = np.where(corrtypes == c)[0][0]
                     corrlbl = ReverseStokesTypes[c]
                     corrfactor_str = f"{origsize[cii]/newsize[cii]:.3f}" if newsize[cii] > 0 else "Fully flagged"
-                    log.info(f"\tCompression factor (CF) for scan {s} {corrlbl}: {corrfactor_str}")
+                    log.info(f"\tData compression factor (CF) for scan {s} {corrlbl}: {corrfactor_str}")
+                total_data_volume = data_volume_calc(num_antennas=len(ANTSEL),
+                                                        num_polarisations=len(CORRSEL),
+                                                        total_obs_hrs=0, #ignored nrow given
+                                                        dump_rate=0, #ignored nrow given
+                                                        num_channels=svds[bli][corrlbl]['shape'][0],
+                                                        auto_correlations=NOSKIPAUTO,
+                                                        nrows=nrows,
+                                                        num_data_cols=0) # not storing a DATA column
+                total_data_volume_with_data = data_volume_calc(num_antennas=len(ANTSEL),
+                                                               num_polarisations=len(CORRSEL),
+                                                               total_obs_hrs=0, #ignored nrow given
+                                                               dump_rate=0, #ignored nrow given
+                                                               num_channels=svds[bli][corrlbl]['shape'][0],
+                                                               auto_correlations=NOSKIPAUTO,
+                                                               nrows=nrows,
+                                                               num_data_cols=1) # not storing a DATA column
+                data_size = total_data_volume_with_data - total_data_volume
+                data_corr_size = data_size / len(CORRSEL)
+                compressed_data_size = np.sum(np.ceil(data_corr_size * (newsize / origsize)))
+                log.info(f"\tTotal new {nrows} row MAIN table meta data volume: "
+                         f"{total_data_volume+compressed_data_size:.0f} bytes")
                 #
                 # STEP 3: decompress and make some waterfall and rank plots if wanted
                 #
